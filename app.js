156142e19879baff2a78f8777d3714485410f5d2
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-27 17:30:38 -0500

    Set before headers a todos los request

--- app.js
+++ app.js
@@ -2,15 +2,35 @@
 
 const Hapi = require('hapi');
 const server = new Hapi.Server();
+const setHeader = require('hapi-set-header');
+
 var routes = require('./configs/routes');
 
 server.connection({ port: 3000, host: 'localhost' });
 
+server.state('session', {  
+    ttl: 1000 * 60 * 60 * 24,    // 1 day lifetime
+    encoding: 'base64json'       // cookie data is JSON-stringified and Base64 encoded
+});
+
+server.state('data', {
+
+});
+
+
+
 server.start((err) => {
-    if (err) {
-        throw err;
-    }
+    if (err) { throw err; }
     console.log(`Server running at: ${server.info.uri}`);
+});
+
+setHeader(server, 'x-powered-by', 'Node.js');
+setHeader(server, 'Content-Type', 'text/plain');
+setHeader(server, 'Server', 'Ubuntu');
+
+server.ext('onPreHandler', function (request, reply) {
+    //request.response.header('X-API-VERSION', '0.0.1');
+    reply.continue();
 });
 
 var index = server.route({
